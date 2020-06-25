315fe4c17834fc97e9221093dcca723dbbd223e5
Alexander <44552657+tehnix53@users.noreply.github.com>
2020-06-25 21:53:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import sys
-import os
-import numpy as np
 import qimage2ndarray
-import pydicom as pyd
-
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QPixmap
+
 import design
 
-from plot_objects import MplCanvas
-
-# type QPIXMAP concert from matplotlib axes!!!
-# image=qimage2ndarray.array2qimage(b)
-# add two layers for image and series
+from pil_object import dicom_to_qt
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -21,24 +14,27 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.setupUi(self)
         self.pushButton_2.clicked.connect(self.browse_folder)
         self.pushButton.clicked.connect(self.load_image)
-        # self.label.setPixmap(QtGui.QPixmap.fromImage(sc))
-        # self.label.setPixmap(sc)
+
         # первые цифры - положение на экране, третья и четвертая - размер окна
         # self.setGeometry(500, 250, 300, 300)
 
     def browse_folder(self):
         file_name = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите папку", None, "Image (*.bin *.dcm)")[0]
         self.lineEdit.setText(file_name)
-        # self.setGeometry(200, 200, 300, 300)
-
-    # QtGui.QPixmap.fromImage(self._image)
 
     def load_image(self):
         file_name = self.lineEdit.text()
-        sc = MplCanvas(width=5, height=4, dpi=100)
-        lung = pyd.dcmread(file_name).pixel_array
-        sc.axes.imshow(lung)
-        self.setCentralWidget(sc)
+        qim = dicom_to_qt(file_name)
+        pix = QPixmap.fromImage(qim)
+        pixmap_resized = pix.scaled(400, 1000, QtCore.Qt.KeepAspectRatio)
+        self.label.setPixmap(pixmap_resized)
+
+
+
+        ## sc = MplCanvas(width=5, height=4, dpi=100)
+        ## lung = pyd.dcmread(file_name).pixel_array
+        ## sc.axes.imshow(lung)
+        ## self.setCentralWidget(sc)
         # pixmap = self.lineEdit.text()
         # self.label.setPixmap(QtGui.QPixmap.fromImage(image))
         # self.setupUi(self)
